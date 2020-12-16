27d7e55d89221c962ef6b28f5b69563f078d7e82
chalalala <neko_hime1212@yahoo.com>
2020-12-16 16:00:43 +0700

    Update dependencies

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/MainActivity.java
@@ -3,8 +3,11 @@ package vn.edu.usth.onlinemusicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
 
     public void setActionBarTitle(String title) {
