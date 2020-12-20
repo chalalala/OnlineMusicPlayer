3ca43b6f669cc694c74686cf1edfe4031db20817
chalalala <neko_hime1212@yahoo.com>
2020-12-20 17:27:22 +0700

    Change display order

--- app/src/main/java/vn/edu/usth/onlinemusicplayer/HomeFragment.java
+++ app/src/main/java/vn/edu/usth/onlinemusicplayer/HomeFragment.java
@@ -3,6 +3,7 @@ package vn.edu.usth.onlinemusicplayer;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,6 +38,31 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home_screen, container, false);
 
+        // Recent - Image
+        ImageView first_img = view.findViewById(R.id.recent_first);
+        ImageView second_img = view.findViewById(R.id.recent_second);
+        ImageView third_img = view.findViewById(R.id.recent_third);
+
+        // Recent - Song name
+        TextView first = view.findViewById(R.id.recent_first_name);
+        TextView second = view.findViewById(R.id.recent_second_name);
+        TextView third = view.findViewById(R.id.recent_third_name);
+
+        // Recent - Artist name
+        TextView first_name = view.findViewById(R.id.recent_artist_first);
+        TextView second_name = view.findViewById(R.id.recent_artist_second);
+        TextView third_name = view.findViewById(R.id.recent_artist_third);
+
+        // Artist name
+        TextView artist_first = view.findViewById(R.id.artist_first_name);
+        TextView artist_second = view.findViewById(R.id.artist_second_name);
+        TextView artist_third = view.findViewById(R.id.artist_third_name);
+
+        TextView[] song_names = {first, second, third};
+        TextView[] recent_artists = {first_name, second_name, third_name};
+        TextView[] artists_name = {artist_first, artist_second, artist_third};
+        ImageView[] images = {first_img, second_img, third_img};
+
         AssetManager assetManager = getContext().getAssets();
 
         try {
@@ -58,38 +84,14 @@ public class HomeFragment extends Fragment {
                 }
             }
 
-            // Recent - Image
-            ImageView first_img = view.findViewById(R.id.recent_first);
-            ImageView second_img = view.findViewById(R.id.recent_second);
-            ImageView third_img = view.findViewById(R.id.recent_third);
-
-            // Recent - Song name
-            TextView first = view.findViewById(R.id.recent_first_name);
-            TextView second = view.findViewById(R.id.recent_second_name);
-            TextView third = view.findViewById(R.id.recent_third_name);
-
-            // Recent - Artist name
-            TextView first_name = view.findViewById(R.id.recent_artist_first);
-            TextView second_name = view.findViewById(R.id.recent_artist_second);
-            TextView third_name = view.findViewById(R.id.recent_artist_third);
-
-            // Artist name
-            TextView artist_first = view.findViewById(R.id.artist_first_name);
-            TextView artist_second = view.findViewById(R.id.artist_second_name);
-            TextView artist_third = view.findViewById(R.id.artist_third_name);
-
-            TextView[] song_names = {first, second, third};
-            TextView[] recent_artists = {first_name, second_name, third_name};
-            TextView[] artists_name = {artist_first, artist_second, artist_third};
-            ImageView[] images = {first_img, second_img, third_img};
-
             for (int i=0; i<song_names.length; i++){
                 String sname = songs.get(i);
                 String aname = artists.get(i);
                 song_names[i].setText(sname);
                 recent_artists[i].setText(aname);
                 artists_name[i].setText(single_artist.get(i));
-
+                Log.e("hello", "File name" + fileNames[i]);
+                Log.e("hello2", "Song name" + songs.get(i));
                 // Handle click
                 images[i].setOnClickListener(new CustomOnClickListener(fileNames[i]) {
                     @Override
